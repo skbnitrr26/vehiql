22b363b8b1be1496df059bcd0be5d10b63129b46
Sumit <skbkdh2017@gmail.com>
2025-08-28 11:38:35 +0530

    updated fine

--- components/home-search.jsx
+++ components/home-search.jsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import { Input } from "./ui/input";
-import { Camera, Upload } from "lucide-react";
+import { Camera, Search, Upload } from "lucide-react";
 import { Button } from "./ui/button";
 import { useDropzone } from "react-dropzone";
 import { toast } from "sonner";
@@ -107,18 +107,19 @@ const HomeSearch = () => {
 
 
   return (
-    <div>
+    <div >
       <form onSubmit={handleTextSubmit}>
         <div className="relative flex items-center">
+          <Search className="absolute left-3 w-5 h-5" />
           <Input
             type="text"
-            placeholder="Enter make, model, or use our AI Image Search..."
+            placeholder="Enter make, model, or use AI Image Search..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
             className="pl-10 pr-12 py-6 w-full rounded-full border-gray-300 bg-white/95 backdrop-blur-sm"
           />
 
-          <div className="absolute right-[100px]">
+          <div className="absolute right-[100px]-translate-y-1/2 cursor-pointer rounded-lg p-1">
             <Camera
               size={35}
               onClick={() => setIsImageSearchActive(!isImageSearchActive)}
